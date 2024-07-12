c2da7ce6cee998ebef311eb4ceb5129a9e25a918
admin <admin@TAHA>
2024-07-13 01:25:25 +0300

    Add project files.

---
+++ MyPortfolioUdemy/Controllers/SkillController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+	public class SkillController : Controller
+	{
+		MyPortfolioContext portfolioContext = new MyPortfolioContext();
+		public IActionResult SkillList()
+		{
+			var value = portfolioContext.Skills.ToList();
+			return View(value);
+		}
+
+		[HttpGet]
+		public IActionResult CreateSkill() 
+		{
+			return View();		
+		}
+		[HttpPost]
+		public IActionResult CreateSkill(Skill skill) 
+		{ 
+			var value = portfolioContext.Skills.Add(skill);
+			portfolioContext.SaveChanges();
+			return RedirectToAction("SkillList", "Skill");
+
+		}
+
+		public IActionResult DeleteSkill(int id)
+		{
+			var values = portfolioContext.Skills.Find(id);
+			portfolioContext.Skills.Remove(values);
+			portfolioContext.SaveChanges();
+			return RedirectToAction("SkillList", "Skill");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateSkill(int id) 
+		{
+			var value = portfolioContext.Skills.Find(id);
+			return View(value);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateSkill(Skill skill)
+		{
+			portfolioContext.Skills.Update(skill);
+			portfolioContext.SaveChanges();
+			return RedirectToAction("SkillList", "Skill");
+		}
+	}
+}
